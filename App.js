cd530b8d82a0c1ded88a0da9ea55b18afebaf615
Meriton <musliumeriton@gmail.com>
2024-11-24 23:44:03 +0100

    add some extra navigation with button

--- App.js
+++ App.js
@@ -2,15 +2,42 @@ import { NavigationContainer } from "@react-navigation/native";
 import { createDrawerNavigator } from "@react-navigation/drawer";
 import WelcomeScreen from "./screens/WelcomeScreen";
 import UserScreen from "./screens/UserScreen";
+import { Ionicons } from "@expo/vector-icons";
 
 const Drawer = createDrawerNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Drawer.Navigator>
-        <Drawer.Screen name="Welcome" component={WelcomeScreen} />
-        <Drawer.Screen name="User" component={UserScreen} />
+      <Drawer.Navigator
+        screenOptions={{
+          drawerActiveBackgroundColor: "#f0f0f0",
+          drawerActiveTintColor: "#2a2a2a",
+          drawerStyle: { backgroundColor: "#ddd" },
+          headerStyle: { backgroundColor: "#e1e1e1" },
+          headerTintColor: "#333d",
+        }}
+      >
+        <Drawer.Screen
+          name="Welcome"
+          component={WelcomeScreen}
+          options={{
+            drawerLabel: "Welcome Screen",
+            drawerIcon: ({ color, size }) => (
+              <Ionicons name="home" color={color} size={size} />
+            ),
+          }}
+        />
+        <Drawer.Screen
+          name="User"
+          component={UserScreen}
+          options={{
+            drawerLabel: "User Screen",
+            drawerIcon: ({ color, size }) => (
+              <Ionicons name="person" color={color} size={size} />
+            ),
+          }}
+        />
       </Drawer.Navigator>
     </NavigationContainer>
   );
